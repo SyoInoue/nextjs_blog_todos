df27160bf913b88535a6e0fc56ae5f22396cd218
SyoInoue <funnyfacee7777@gmail.com>
2021-03-27 14:15:26 +0900

    footer changed

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children, title = "Default title" }) {
         {children}
       </main>
       <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
-        @SyoInoue 2021
+        @Next.js 2021
       </footer>
     </div>
   );
